4d1e5a5f5475f94eb11c866f12d8463b18c2197e
QuincyMashava <mashavaquincy@gmail.com>
2025-09-28 12:16:04 +0200

    feat: setup project 0x05

---
+++ alx-project-0x05/interface/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+
+
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+
+export interface PageRouteProps {
+  pageRoute: string
+}
